b41906e46fc9fc4dd46b9f42eb8250652677a327
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-09-15 13:52:14 +0100

    simple example of REST call

--- apiClients/sitelistApiClient.js
+++ apiClients/sitelistApiClient.js
@@ -1,11 +1,24 @@
-import BaseApiClient from './baseApiClient.js'
 
-const ENDPOINT = 'sitelist';
+// example of a REST call
 
-export default class SitelistApiClient extends BaseApiClient {
+// We should refactor the URL assembly code so that all our calls can use ut
+import { URL } from 'url';
+import axios from 'axios';
+
+const BASE_URL = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/';
+const API_KEY = 'bf5f5026-1766-49c3-9d9f-aff7ae95a8e6';
+
+export default class SitelistApiClient {
+    requestUrl(endpoint) {
+        const requestUrl = new URL(endpoint, BASE_URL);
+        requestUrl.searchParams.append("key", API_KEY);
+        return requestUrl.href;
+    }
     getLocations() {
-        return this.makeRequest(ENDPOINT).then ( 
-                body =>  body.Locations.Location              
-            );  
+            return axios.get(this.requestUrl('sitelist')).then(              
+                result => {
+                    return  result.data.Locations.Location;
+                }
+            );
     }
 }
